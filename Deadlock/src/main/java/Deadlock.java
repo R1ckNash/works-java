a5b05e4213e467719d33d926137b9297d3fe2c8e
R1ckNash <Carapaceeee@rambler.ru>
2018-10-22 10:10:50 +0300

    Initial commit

--- Deadlock/src/main/java/Deadlock.java
+++ Deadlock/src/main/java/Deadlock.java
@@ -5,20 +5,20 @@ public class Deadlock {
 
         private final String name;
 
-        public Friend(String name) {
+        private Friend(String name) {
             this.name = name;
         }
 
-        public String getName() {
+        private String getName() {
             return this.name;
         }
 
-        public synchronized void bow(Friend bower) {
+        private synchronized void bow(Friend bower) {
             System.out.format("%s: %s" + "  has bowed to me!%n", this.name, bower.getName());
             bower.bowBack(this);
         }
 
-        public synchronized void bowBack(Friend bower) {
+        private synchronized void bowBack(Friend bower) {
             System.out.format("%s: %s" + " has bowed back to me!%n", this.name, bower.getName());
         }
     }
